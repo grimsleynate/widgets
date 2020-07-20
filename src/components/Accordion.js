e7e69826d4b01d48b3b21aebf2601da367ac32da
Nathaniel Grimsley <grimsleynate@gmail.com>
2020-07-20 15:28:45 -0400

    udpate

---
+++ src/components/Accordion.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Accordion = ({items}) => {
+    const renderedItems = items.map((item, index) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div 
+                    className="title active"
+                    onClick={() => console.log(`Title Clicked: #${index}`)}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        );
+    });
+
+    return (
+        <div className="accordion ui styled">
+            {renderedItems}
+        </div>  
+    );
+};
+
+export default Accordion;
