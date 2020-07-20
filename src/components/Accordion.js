cb2328b46d896a7b1095d2b0284ece286c8ff8e5
Nathaniel Grimsley <grimsleynate@gmail.com>
2020-07-20 15:35:36 -0400

    built helper function onTitleClick

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Accordion = ({items}) => {
+    //helper function that is triggered when the user 
+    //clicks on an item.
+    const onTitleClick = (index) => {
+        console.log('Title clicked', index);
+    };
+
+    //This maps each item in the items props to a list
     const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
                 <div 
                     className="title active"
-                    onClick={() => console.log(`Title Clicked: #${index}`)}>
+                    onClick={() => onTitleClick(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
