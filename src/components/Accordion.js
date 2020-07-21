fe49439925f7525a2be219358349982ae2813730
Nathaniel Grimsley <grimsleynate@gmail.com>
2020-07-21 10:34:09 -0400

    Finished implementing Accordion widget

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,29 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
+//This component creates an accordion-style list.  
+/*PROPS NEEDED: 
+    items: An array of objects that contain two key-value
+        pairs with the keys "title" and "content" */
 const Accordion = ({items}) => {
+    //this state will store which index in our list of items
+    //is currently active.
+    const [ activeIndex, setActiveIndex] = useState(null);
+
     //helper function that is triggered when the user 
     //clicks on an item.
     const onTitleClick = (index) => {
-        console.log('Title clicked', index);
+        setActiveIndex(index);
     };
 
     //This maps each item in the items props to a list
     const renderedItems = items.map((item, index) => {
+        //if the current index is active (according to the activeIndex state),
+        //store the string 'active' in the variable 'active.  Otherwise, store an empty string.
+        const active = index === activeIndex ? 'active' : '';
+        
         return (
             <React.Fragment key={item.title}>
                 <div 
-                    className="title active"
-                    onClick={() => onTitleClick(index)}>
+                    className={`title ${active}`}
+                    onClick={() => onTitleClick(index)}
+                >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
         );
     });
 
+    //We are returning a div containing our rendered list of items.
     return (
         <div className="accordion ui styled">
             {renderedItems}
